bd89a39121f3d7fb062846e82f76922428546680
NadyaSlb <nslabuk1@email.cpcc.edu>
2024-01-13 17:26:09 -0500

    parts 3-6

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # belly-button-challenge
+
+## Description
+
+In this assignment, I've built an interactive dashboard to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.
+
+The following tools were used in this work: JavaScript, Plotly, and d3.
--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,8 @@
 let object;
 let menuNames;
 let topTenSamples;
+let initialDiv = d3.select('#sample-metadata');
+
 //Use the D3 library to read in samples.json from the URL
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 d3.json(url).then(function(data) {
@@ -9,7 +11,9 @@ d3.json(url).then(function(data) {
     menuNames = object.names;
     console.log(object);
     dropdownMenu();
-    init();
+    initBarChart();
+    initBubbleChart();
+    initMetadata();
   });
 
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
@@ -17,13 +21,6 @@ d3.json(url).then(function(data) {
 function dropdownMenu(){
     let selector = d3.selectAll('#selDataset');
     menuNames.map((menuName) => {selector.append('option').attr('value', menuName).text(menuName)});
-    //selector.on("change", function () {
-        // Get the selected value
-        //let selectedValue = d3.select(this).property("value");
-       // console.log(selectedValue);
-        // Call a function or perform actions based on the selected value
-      //  optionChanged(selectedValue);
-   // });
 }
 
 //function to extract top10 info
@@ -36,14 +33,11 @@ function extractTopTenInfo(sample) {
     };
 }
 
-
 // Display the default plot
-function init() {
+function initBarChart() {
     let samples = object.samples;
     //top10 sample values
     topTenSamples = samples.map(extractTopTenInfo);
-    //console.log(topTenSamples);
- 
 
     let data = [{
       x: topTenSamples[0].ten_sample_values,
@@ -52,28 +46,28 @@ function init() {
       type: "bar",
       orientation: 'h'
     }];
-  console.log(data);
     let layout = {
       height: 500,
       width: 600,
       yaxis: {
-        autorange: 'reversed' // Reverse the order of OTUs for better visualization
+        autorange: 'reversed'
     }
     };
   
     Plotly.newPlot("bar", data, layout);
   }
 
-// Update the restyled plot's values
+// Update the values
 function optionChanged(selectedValue) {
-    console.log(topTenSamples);
-    console.log(selectedValue);
     let selectedSample = topTenSamples.find(sample => sample.id === selectedValue.toString());
-    console.log(selectedSample);
-    updateChart(selectedSample);
+    updateBarChart(selectedSample);
+    let selectedSampleBubble = object.samples.find(sample => sample.id === selectedValue.toString());
+    updateBubbleChart(selectedSampleBubble);
+    let selectedSampleMeta = object.metadata.find(sample => sample.id === parseInt(selectedValue));
+    updateMetadata(selectedSampleMeta);
 };
 
-function updateChart(selectedSample) {
+function updateBarChart(selectedSample) {
     let updatedData = [{
         x: selectedSample.ten_sample_values,
         y: selectedSample.ten_otu_ids.map(id => `OTU ${id}`),
@@ -81,7 +75,6 @@ function updateChart(selectedSample) {
         type: "bar",
         orientation: 'h'
     }];
-console.log(updatedData);
     let layout = {
         height: 500,
         width: 600,
@@ -94,4 +87,76 @@ console.log(updatedData);
     Plotly.react("bar", updatedData, layout);
 }
 
+//Create a bubble chart 
+function initBubbleChart() {
+    let samples = object.samples;
+    let data = [{
+        x: samples[0].otu_ids,
+        y: samples[0].sample_values,
+        mode: 'markers',
+        marker: {
+            size: samples[0].sample_values,
+            color: samples[0].otu_ids,
+            colorscale: 'Viridis'
+        },
+        text: samples[0].otu_labels
+    }]
+
+    let layout = {
+        showlegend: false,
+        height: 500,
+        width: 1000,
+        xaxis: {
+            title: 'OTU ID'
+        }
+    };
+
+    Plotly.newPlot("bubble", data, layout);
+}
+
+function updateBubbleChart(selectedSampleBubble) {
+    let updatedData = [{
+        x: selectedSampleBubble.otu_ids,
+        y: selectedSampleBubble.sample_values,
+        mode: 'markers',
+        marker: {
+            size: selectedSampleBubble.sample_values,
+            color: selectedSampleBubble.otu_ids,
+            colorscale: 'Viridis'
+        },
+        text: selectedSampleBubble.otu_labels
+    }];
+    let layout = {
+        showlegend: false,
+        height: 500,
+        width: 1000,
+        xaxis: {
+            title: 'OTU ID'
+        }
+    };
+
+    // Update the chart with the new data
+    Plotly.react("bubble", updatedData, layout);
+}
   
+//Display each key-value pair from the metadata JSON object
+function initMetadata(){
+    initialDiv.append("p").text('id: ' + object.metadata[0].id);
+    initialDiv.append("p").text('ethnicity: ' + object.metadata[0].ethnicity);
+    initialDiv.append("p").text('gender: ' + object.metadata[0].gender);
+    initialDiv.append("p").text('age: ' + object.metadata[0].age);
+    initialDiv.append("p").text('location: ' + object.metadata[0].location);
+    initialDiv.append("p").text('bbtype: ' + object.metadata[0].bbtype);
+    initialDiv.append("p").text('wfreq: ' + object.metadata[0].wfreq);
+};
+
+function updateMetadata(selectedSampleMeta){
+    initialDiv.html("");
+    initialDiv.append("p").text('id: ' + selectedSampleMeta.id);
+    initialDiv.append("p").text('ethnicity: ' + selectedSampleMeta.ethnicity);
+    initialDiv.append("p").text('gender: ' + selectedSampleMeta.gender);
+    initialDiv.append("p").text('age: ' + selectedSampleMeta.age);
+    initialDiv.append("p").text('location: ' + selectedSampleMeta.location);
+    initialDiv.append("p").text('bbtype: ' + selectedSampleMeta.bbtype);
+    initialDiv.append("p").text('wfreq: ' + selectedSampleMeta.wfreq);
+}
